ac7b71fb17b4e09be1795d150d8bb02dddc3fa31
HaDeSMonsta <leon.poellinger+git@gmail.com>
2024-02-19 04:07:26 +0100

    Add logging functionality and dependencies
    The code is updated to add new logging functionality. Functions for logging messages with timestamps to stdout and files have been introduced. In addition, the 'chrono' dependency for timestamp management has been added. Tests and a previous 'add' function were removed to focus on these logging functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fs::OpenOptions;
+use std::io::{BufWriter, Result, Write};
+
+use chrono::Utc;
+
+/// This function logs the given message with the current UTC time string.
+///
+/// # Arguments
+///
+/// * `to_log` - The message to be logged.
+pub fn log(to_log: &str) {
+    println!("{}", mk_str(to_log));
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Write a log message to a file.
+///
+/// # Arguments
+///
+/// * `to_log` - The log message to write.
+/// * `file_name` - The name of the log file to write to.
+///
+/// # Panics
+///
+/// This function will panic if it is unable to open the log file.
+///
+/// # Errors
+///
+/// This function will return an error if it is unable to write to the log file.
+pub fn log_file(to_log: &str, file_name: &str) -> Result<()> {
+    let file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(file_name)
+        .expect("Unable to open logfile");
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let mut writer = BufWriter::new(file);
+
+    let msg = mk_str(to_log);
+
+    write!(writer, "{msg}")?;
+    Ok(())
+}
+
+/// Returns a string slice with a formatted log message.
+///
+/// # Arguments
+///
+/// * `to_log` - The message to include in the log.
+///
+/// # Examples
+fn mk_str(to_log: &str) -> String {
+    let now = Utc::now()
+        .format("[%Y-%m-%d] - [%h:%M-%S]")
+        .to_string();
+    let msg = format!("{now} - {to_log}");
+
+    msg
 }
